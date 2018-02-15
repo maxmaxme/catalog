912ea5e9981167c18726e2e914f304fb2cbf415d
Maxim Lepekha <m.lepekha@yandex.ru>
2018-02-15 23:14:22 +0300

    сортировка, функция для выборки товаров, стили

--- functions/goods.php
+++ functions/goods.php
@@ -19,3 +19,69 @@ function getGoodBlock($good) {
 HTML;
 
 }
+
+function getMoreBlock() {
+	return <<<HTML
+	
+		<div class="good_more_button">
+			<div class="plus">
+				+
+			</div>
+			<div>Загрузить еще 50</div>
+		</div>
+
+HTML;
+
+}
+
+
+/**
+ * Возвращает список товаров
+ * @param int $page страница. от 1
+ * @param int $order имя столбца для сортировки
+ * @param int $orderType ASC/DESC
+ * @return array
+ */
+function getGoods($page, $order, $orderType) {
+
+	$mysqli = getMysqli();
+
+	$perPage = 50;
+
+	$limit = ($page - 1) * $perPage;
+
+	$limit = $limit > 0 ? $limit : 0;
+
+
+	$goods = $mysqli->query("
+		select
+				SQL_CALC_FOUND_ROWS
+				g.ID,
+				g.Name,
+				g.Description,
+				g.PhotoURL,
+				g.Price
+			from goods g
+			
+		WHERE
+			1
+			
+		ORDER BY 
+			g.{$order} {$orderType}
+			
+		LIMIT 
+			{$limit}, {$perPage}
+	  
+	");
+
+	$total_count =
+		$mysqli->query('select FOUND_ROWS()')->fetch_row()[0];
+
+	$more =
+		$total_count > $limit + $perPage;
+
+	return [
+		'items' => $goods->fetch_all(MYSQLI_ASSOC),
+		'more' =>  $more
+	];
+}
--- public/index.php
+++ public/index.php
@@ -2,38 +2,75 @@
 
 require '../config.php';
 
-$mysqli = getMysqli();
+$orders = [
+	'ID' => 'ID',
+	'Price' => 'Цене'
+];
 
-$goods = $mysqli->query("
-	select
-			g.ID,
-			g.Name,
-			g.Description,
-			g.PhotoURL,
-			g.Price
-		from goods g
-		
-	WHERE
-		1
-		
-	ORDER BY 
-		g.ID
-		
-	LIMIT 
-		0, 50
-  
-");
+$orderTypes = ['ASC', 'DESC'];
 
-$content = '<div class="container">';
+$curOrder = in_array($_GET['order'], array_keys($orders)) ? $_GET['order'] : array_keys($orders)[0];
+$curOrderType = in_array($_GET['orderType'], $orderTypes) ? $_GET['orderType'] : $orderTypes[0];
 
-while($good = $goods->fetch_assoc()) {
-	$content .= getGoodBlock($good);
-}
 
-$content .= '</div>';
+$goods = getGoods(1, $curOrder, $curOrderType);
 
+
+if ($goods['items']) {
+
+	// формирование кнопок для сортировки
+	$ordersList = implode(' или ', array_map(function ($key) use ($orders, $orderTypes, $curOrder, $curOrderType) {
+
+		$class = '';
+
+		// Если это текущая сортировка — меняем ASC на DESC и наоборот
+		if ($key == $curOrder) {
+
+			$curTypeID = array_search($curOrderType, $orderTypes);
+			unset($orderTypes[$curTypeID]);
+
+			$class = 'selected';
+
+		}
+
+		$type = array_shift($orderTypes);
+
+		$order = '?order=' . $key . '&orderType=' . $type;
+
+		return "<a class='{$class}' href='{$order}'>{$orders[$key]}</a>";
+
+	}, array_keys($orders)));
+
+
+	$content = <<<HTML
+
+		<div class="order">
+			<b>Сортировать по:</b>
+			{$ordersList}
+		</div>
+	
+		<div class="goods">
+HTML;
+
+	$content .= implode('', array_map(function ($good) {
+		return getGoodBlock($good);
+	}, $goods['items']));
+
+	if (0 && $goods['more'])
+		$content .= getMoreBlock();
+
+	$content .= '</div>';
+
+} else
+	$content = 'Ничего не найдено';
 
 echo getTemplate('base', [
 	'title' => 'Список товаров',
+	'pageTitle' => 'Список товаров',
 	'content' => $content
 ]);
+
+
+
+
+echo '<div id="page_load_time">' . round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 5) . '</div>';
