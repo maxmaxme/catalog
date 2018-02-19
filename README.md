05355800d253665235b9bb63af8672ea6ca1aee3
Maxim Lepekha <m.lepekha@yandex.ru>
2018-02-19 21:52:59 +0300

    generate goods script

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Корень сайта в /public/
 - В /config/config.server.php прописать настройки БД и Memcached
 - Поддерживаются имена конфигов вида config.server._HOST_.php. _HOST_ получен  через gethostname()
+- Генерация товаров через /generate.php; за раз 10k товаров
---
+++ public/generate.php
@@ -0,0 +1,65 @@
+<?php
+require '../config.php';
+
+
+$names = [
+	[
+		'Супер',
+		'Новый',
+		'Потрясающий',
+		'Великолепный',
+		'Невероятный',
+	],
+	[
+		'фен',
+		'телевизор',
+		'телефон',
+		'ноутбук',
+		'пылесос',
+		'холодильник',
+		'блендер',
+		'утюг',
+	],
+	[
+		'Филиппс',
+		'Панасоник',
+		'Самсунг',
+		'Сони',
+		'Эппл',
+		'Сяоми',
+		'Ксяоми',
+		'Салями',
+	],
+	[
+		100,
+		200,
+		300,
+		400,
+		500,
+		600,
+		700,
+		9000
+	]
+];
+
+
+$mysqli = getMysqli();
+
+for ($i = 1; $i <= 10000; $i++) {
+
+
+	$name = implode(' ', array_map(function ($type) {
+		return $type[rand(0, count($type) - 1)];
+	}, $names));
+
+	$description = 'Описание для ' . $name;
+
+	$price = rand(1, 999) * 100;
+
+	$photo = 'https://picsum.photos/300?random';
+
+	$sql = "insert into goods set Name='{$name}', Description='{$description}', Price='{$price}', PhotoURL='{$photo}'";
+
+	$mysqli->query($sql);
+
+}
